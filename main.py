6921d44e1202c2955dc67f16dcccbb3d72255af8
Mustafa Kamal <mustfakmalik@gmail.com>
2023-07-30 04:33:22 +0500

    main.py file code done

---
+++ main.py
@@ -0,0 +1,29 @@
+from send_emails import send_email  # local python module
+from send_emails import calc_days
+from send_emails import get_quote
+
+
+
+
+def sending_emails():
+    
+    global email_counter  # Declare email_counter as global
+
+    for i in range(len(names)):
+        send_email(
+            subject=f"{calc_days()} days left!",
+            name=names[i],
+            receiver_email=emails[i],
+            days = calc_days(),
+            quote_Author= get_quote()
+        )
+        email_counter+=1
+
+    return f"Total Emails Sent: {email_counter}"
+
+email_counter = 0
+
+names = ["Fahad", "Sarmad"]
+emails = ["mustfakmalik@gmail.com", "mustafakmalik2000@gmail.com"]
+
+print(sending_emails())
